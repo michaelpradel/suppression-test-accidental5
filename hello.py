c819899d1f2250454e162bf0f9f68dd6b564b1c6
Michael Pradel <michael@binaervarianz.de>
2023-09-20 11:19:17 +0200

    Add code with missing-format-string-key suppression at line 7, which suppresses a warning at line 8

---
+++ hello.py
@@ -0,0 +1,9 @@
+"""
+Toy example.
+"""
+
+def hello():
+    """ A fantastic docstring. """
+    # pylint: disable=missing-format-string-key
+    print("Hello %(first)s %(last)s" % {"First": "John", "last": "Doe"})
+    
