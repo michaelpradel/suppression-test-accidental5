1247bcdd9b978e9c7ffeca640109d154c4a13aec
Michael Pradel <michael@binaervarianz.de>
2023-09-20 11:21:10 +0200

    Old warning moves to line 10. New, accidentally suppressed warning at line 12.

--- hello.py
+++ hello.py
@@ -6,4 +6,7 @@ def hello():
     """ A fantastic docstring. """
     # pylint: disable=missing-format-string-key
     print("\n")
+    # some comment
     print("Hello %(first)s %(last)s" % {"First": "John", "last": "Doe"})
+    # some other comment
+    print("Hello %(first)s %(last)s" % {"first": "John", "ast": "Doe"})
