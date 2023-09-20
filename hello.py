d1542a98d38e145d2ef94546fb0e2be594511eb0
Michael Pradel <michael@binaervarianz.de>
2023-09-20 11:19:45 +0200

    Add code that moves the suppressed warning to line 9

--- hello.py
+++ hello.py
@@ -5,5 +5,5 @@ Toy example.
 def hello():
     """ A fantastic docstring. """
     # pylint: disable=missing-format-string-key
+    print("\n")
     print("Hello %(first)s %(last)s" % {"First": "John", "last": "Doe"})
-    
